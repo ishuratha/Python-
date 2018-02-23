5b1e4d4a9de04593da112e571c84b556badee216
ishuratha <ishwariyat23@gmail.com>
2018-02-23 14:38:27 +0530

    Create largest number.py

---
+++ pos or neg.py/largest number.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the value of a:\n")
+b=int(input("Enter the value of b:\n")
+c=int(input("Enter the value of c:\n")
+if((a>b)and(a>c)):
+print(a)
+elif(c>b):
+print(c)
+else:
+print(b)
