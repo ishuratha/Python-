64e7c1476557032c3b934550d17e7b406c978b73
ishuratha <ishwariyat23@gmail.com>
2018-03-20 16:07:04 +0530

    Create arm s r no.py

---
+++ pos or neg.py/arm s r no.py
@@ -0,0 +1,12 @@
+a=int(raw_input())
+sum=0
+temp=n
+while(temp>0):
+digit=temp%10
+sum=sum+digit**3
+temp/=10
+if(n==sum):
+print("It is an armstrong number")
+else:
+print("It is not an armstrong number")
+
